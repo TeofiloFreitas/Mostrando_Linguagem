d48cab2b81e8974b2ee4782fadba6ac1e6fe0748
TeofiloFreitas <59712317+TeofiloFreitas@users.noreply.github.com>
2020-09-08 15:18:47 -0300

    Upload file

---
+++ Linguagem/src/Lingua.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class Lingua {
+
+	public static void main(String[] args) {
+		
+		Locale loc = Locale.getDefault();
+		
+		System.out.println("A lingua usada no sistema é: " + loc.getDisplayLanguage());
+		
+
+	}
+
+}
